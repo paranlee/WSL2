a2293242cb5ba4811d0fb133d86da35b65e166ba
Paran Lee <paran.lee@mail.uk>
2021-02-25 23:54:31 +0900

    Create arm64-linux-cross-compile.sh
    add for arm64 cross compiling

---
+++ arm64-linux-cross-compile.sh
@@ -0,0 +1,4 @@
+export ARCH=arm64
+# https://developer.arm.com/tools-and-software/open-source-software/developer-tools/gnu-toolchain/gnu-a/downloads
+# export PATH=/opt/linaro/gcc-arm-8.3-2019.03-x86_64-aarch64-linux-gnu/bin:$PATH
+export CROSS_COMPILE=aarch64-linux-gnu-
